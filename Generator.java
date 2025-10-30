31c7604dc3fca7394b67b410e84c7425ce3c18d5
Toon Paul <hudallap@gmail.com>
2025-10-30 18:07:46 +0000

    start

--- Generator.java
+++ Generator.java
@@ -1,1 +1,23 @@
+//Name: Paul Hudalla
+//Date:
+//This program draws shapes using loops
 
+public class Generator{
+    public static void stars(int rows){
+        while(rows > 0){
+            System.out.println("*");
+        }
+    }
+    public static void triangle(int rows){
+
+    }
+    public static void odds(int start){
+
+    }
+    public static void even (int maxE){
+
+    }
+    public static void pyramid(int rows){
+
+    }
+}
