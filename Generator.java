59950759eaed3f9c730814fe3bccc826987177e8
Toon Paul <hudallap@gmail.com>
2025-11-05 19:20:34 +0000

    fun2

--- Generator.java
+++ Generator.java
@@ -16,19 +16,24 @@ public class Generator{
     public static void triangle(int rows){
         int num = 1;
         while(rows > 0){
+        int counts = num;    
                 do{
                     System.out.print(num);
-                } while(/*something*/);
-                rows--;
-                System.out.println("");
-                num += 1;
+                    counts--;
+                } while(counts > 0);
+            rows--;
+            System.out.println("");
+            num += 1;
             }
     }
     public static void odds(int start){
 
     }
-    public static void even (int maxE){
-
+    public static void eo (int maxE){
+        /*String letter = "E";
+        if(maxE%2 == 0){
+            letter = "o";
+        }*/
     }
     public static void pyramid(int rows){
 
