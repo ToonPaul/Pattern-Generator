7e24e2c2eaaa2bbbf7a5c6224f793c4ec2b2c8d9
Toon Paul <hudallap@gmail.com>
2025-11-06 19:26:12 +0000

    inpro4

--- Generator.java
+++ Generator.java
@@ -2,59 +2,72 @@
 //Date:
 //This program draws shapes using loops
 
-public class Generator{
-    public static void stars(int rows){
+public class Generator {
+    public static void stars(int rows) {
         String star = "*";
-        while(rows > 0){
-                System.out.print(star);
-                rows--;
-                System.out.println("");
-                star += "**";
-            }
+        while (rows > 0) {
+            System.out.print(star);
+            rows--;
+            System.out.println("");
+            star += "**";
         }
-    //hint: the following functions require more than one loop - Mr. M
-    public static void triangle(int rows){
+    }
+
+    // hint: the following functions require more than one loop - Mr. M
+    public static void triangle(int rows) {
         int num = 1;
-        while(rows > 0){
-        int counts = num;    
-                do{
-                    System.out.print(num);
-                    counts--;
-                } while(counts > 0);
+        while (rows > 0) {
+            int counts = num;
+            do {
+                System.out.print(num);
+                counts--;
+            } while (counts > 0);
             rows--;
             System.out.println("");
             num += 1;
-            }
+        }
     }
-    public static void odds(int start){
-        //for loop
+
+    public static void odds(int start) {
+        // for loop
         int counts = start;
         int num = start;
-        for(int i = start; i > 0;i--){
+        for (int i = start; i > 0; i--) {
             counts = num;
-            if(i%2 == 1){
-                do{
+            if (i % 2 == 1) {
+                do {
                     System.out.print(i);
                     counts--;
-                } while(counts > 0);
-            System.out.println("");
-            } 
+                } while (counts > 0);
+                System.out.println("");
+            }
             num--;
         }
+    }
+
+    public static void eo(int maxE) {
+        // choose any type of loop
+        String letter = "E";
+        int num = maxE;
+        for(int i = maxE; i > 0; i--){
+            if (maxE % 2 == 0) {
+                letter = "O";
+            } else {
+                letter = "E";
         }
-    public static void eo (int maxE){
-        //choose any type of loop
-        /*String letter = "E";
-        if(maxE%2 == 0){
-            letter = "o";
-        }*/
+            
     }
-    public static void pyramid(int rows){
-        //choose any type of loop
     }
-    public static void main (String [] args){
-        //stars(5);
-        //triangle(5);
-        odds(9);
+
+    public static void pyramid(int rows) {
+        // choose any type of loop
+        rows = rows * 2 - 1;
+    }
+
+    public static void main(String[] args) {
+        // stars(5);
+        // triangle(5);
+        // odds(9);
+        eo(6);
     }
 }
