f05ac301f53f7c901add95d0b4db470c66a1e4fd
Toon Paul <hudallap@gmail.com>
2025-11-07 16:57:52 +0000

    final

--- Generator.java
+++ Generator.java
@@ -1,8 +1,9 @@
 //Name: Paul Hudalla
-//Date:
-//This program draws shapes using loops
+//Date: 11/7/25
+//This program draws various shapes using while, do while, and for loops
 
 public class Generator {
+    //Precondition: makes sure to pass a number argument to rows when calling this method
     public static void stars(int rows) {
         String star = "*";
         while (rows > 0) {
@@ -12,8 +13,9 @@ public class Generator {
             star += "**";
         }
     }
+    //Postcondition: draws a triangle made out of * symbols
 
-    // hint: the following functions require more than one loop - Mr. M
+    //Precondition: makes sure to pass a number argument to rows when calling this method
     public static void triangle(int rows) {
         int num = 1;
         while (rows > 0) {
@@ -27,7 +29,9 @@ public class Generator {
             num += 1;
         }
     }
+    //Postcondition: makes a triangle out of numbers that represent the row number
 
+    //Precondition: makes sure to pass a number argument to start when calling this method
     public static void odds(int start) {
         // for loop
         int counts = start;
@@ -44,53 +48,74 @@ public class Generator {
             num--;
         }
     }
+    //Postcondition: makes a triangle out of decending odd numbers
 
+    //Precondition: makes sure to pass a number argument to maxE when calling this method
     public static void eo(int maxE) {
         // choose any type of loop
         int printOut = 1;
         int storage;
         String letter = "E";
-        for(int i = maxE; i > 0; i--){
+        for (int i = maxE; i > 0; i--) {
             storage = printOut;
             if (i % 2 == 1) {
                 letter = "E";
             } else {
                 letter = "O";
             }
-            do{
+            do {
                 System.out.print(letter);
                 storage--;
-            } while(storage > 0);
+            } while (storage > 0);
             System.out.println("");
             printOut += 1;
         }
-       printOut-=2;
-       maxE--;
-        for(int i = maxE; i > 0; i--){
+        printOut -= 2;
+        maxE--;
+        for (int i = maxE; i > 0; i--) {
             storage = printOut;
             if (i % 2 == 1) {
                 letter = "E";
             } else {
                 letter = "O";
             }
-            do{
+            do {
                 System.out.print(letter);
                 storage--;
-            } while(storage > 0);
+            } while (storage > 0);
             System.out.println("");
             printOut -= 1;
         }
     }
+    //Postcondition: makes an isoscles triangle using the letter E and O, depending on if the row is even or odd
 
+    //Precondition: makes sure to pass a number argument to rows when calling this method
     public static void pyramid(int rows) {
         // choose any type of loop
-        //rows = rows * 2 - 1;
+        int num = 1;
+        int counts = rows * 2 - 1;
+        while (rows > 0) {
+            counts = rows * 2 - 1;
+            int spaces = num-1;
+            while(spaces > 0){
+                System.out.print(" ");
+                spaces--;
+            }
+            do {
+                System.out.print(num);
+                counts--;
+            }while(counts > 0);
+            System.out.println("");
+            num++;
+            rows--;
+        }
     }
-
+    //Postcondition: makes a pyramid with the lowest digit on top and the highest digit on the bottom
     public static void main(String[] args) {
-        // stars(5);
-        // triangle(5);
-        // odds(9);
+        stars(5);
+        triangle(5);
+        odds(9);
         eo(5);
+        pyramid(5);
     }
 }
